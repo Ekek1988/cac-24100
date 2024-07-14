51673ff5e5801ba3e18150971f1c86982d589c37
Valentina Thiwissen <vthiwissen@frba.utn.edu.ar>
2024-07-14 20:03:57 -0300

    Cambios

--- js/post.js
+++ js/post.js
@@ -1,29 +1,12 @@
+// cuando el dom se cargue
 document.addEventListener('DOMContentLoaded', async () => {
-
-
-    document.addEventListener("DOMContentLoaded", () => {
-
-        document.getElementById("elementosForm").addEventListener("submit", (event) => {
-            event.preventDefault();
-            validarCampos();
-        });
+    // tengo que levantar los datos del formulario, validar los inputs, en caso de algun value vacio, mostrar un mensaje de error
+    // y no hacer la peticion fetch
+    // si los datos son correctos, realizo la peticion fetch post a la api para agregar una pelicula
+    // si la respuesta es correcta, muestro un mensaje de exito y limpio los inputs del formulario
     
-        document.querySelectorAll(".input").forEach((input) => {
-            input.addEventListener("keydown", (event) => {
-                if (event.key === "Tab" || event.key === "Enter") {
-                    event.preventDefault();
-                    validarCampos();
-                }
-            });
-        });
-    
-    });
-
-
-
-
     //obtengo el formulario
-    formNuevaPelicula = document.getElementById('elementosForm');
+    formNuevaPelicula = document.getElementById('formNuevaPelicula');
     //agrego el evento submit al formulario
     formNuevaPelicula.addEventListener('submit', async (event) => {
         //prevengo el comportamiento por defecto del formulario
@@ -32,48 +15,37 @@ document.addEventListener('DOMContentLoaded', async () => {
         const formData = new FormData(formNuevaPelicula);
         //obtengo los valores de los inputs
         const titulo = formData.get('titulo');
-        const fechaLanzamiento = formData.get('fechaLanzamiento');
         const genero = formData.get('genero');
         const duracion = formData.get('duracion');
-        const director = formData.get('director');
-        const reparto = formData.get('reparto');
-        const sinopsis = formData.get('sinopsis');
         const imagen = formData.get('imagen');
         //valido los inputs
-        if (titulo === ''|| fechaLanzamiento === '' || genero === '' || duracion === '' || director === '' || reparto === '' || sinopsis === '' || imagen === '') {
+        if (titulo === '' || genero === '' || duracion === '' || imagen === '') {
             alert('Todos los campos son obligatorios');
             return;
         }
         // levanto solo el nombre del file para enviarlo a la api
         const imageName = imagen.name;
-       const request = JSON.stringify({
-        titulo: titulo,
-        fechaLanzamiento: fechaLanzamiento,
-        genero: genero,
-        duracion: duracion,
-        director: director,
-        reparto: reparto,
-        sinopsis: sinopsis,
-        imagen: imageName
-    })
-    console.log(request)
+      
         //configuracion de options, es un post y necesita body
         const options = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            mode: 'no-cors',
-            body: request
+            body: JSON.stringify({
+                titulo: titulo,
+                genero: genero,
+                duracion: duracion,
+                imagen: imageName
+            })
         };
-        try{
         //realizo la peticion fetch a la api para agregar una pelicula
         const response = await fetch('http://localhost:8080/apisimple/peliculas', options);
         //obtengo la respuesta
         const data = await response.json();
         //si la respuesta es correcta, muestro un mensaje de exito y limpio los inputs del formulario
         // si el codigo es 201, la pelicula se agrego correctamente
-        if (response.status === 200) {
+        if (response.status === 201) {
             alert('Pelicula agregada correctamente');
             formNuevaPelicula.reset();
             // que se recargue la pagina para ver la pelicula agregada
@@ -81,9 +53,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         } else {
             alert('Error al agregar la pelicula');
         }
-    }catch(error){
-        
-       }
+       
     });
 
 });
